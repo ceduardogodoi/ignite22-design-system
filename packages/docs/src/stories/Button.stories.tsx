a41913ab5711916782c308b8990bed158ddc99a0
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-10-13 22:15:15 -0300

    Documentação de tokens

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -3,7 +3,7 @@ import type { ButtonProps } from '@ignite-ui/react'
 import { Button } from '@ignite-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
 
   args: {
