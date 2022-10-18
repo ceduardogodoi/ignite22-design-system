1c00ea0d65323cfbdc3ec15d53b4a99b31c0106b
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-10-17 22:25:33 -0300

    Componente: Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus quidem sapiente temporibus ipsam beatae harum porro et ipsa assumenda aliquam voluptate commodi esse veniam, unde ab odio exercitationem sunt eius!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
